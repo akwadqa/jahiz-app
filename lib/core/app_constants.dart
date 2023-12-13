094f453dc7374b1c2f197e61f1d387a981dd0685
SaraAkwad <sarak@akwad.qa>
2023-12-13 22:34:56 +0300

    Default host url.

--- lib/core/app_constants.dart
+++ lib/core/app_constants.dart
@@ -1,7 +1,6 @@
 abstract class AppConstants {
   static const String appTitle = 'Jahiz';
-  // static const String hostUrl = 'https://jahiz-new.akwad.qa';
-  static const String hostUrl = 'https://gp.akwad.qa';
+  static const String hostUrl = 'https://jahiz-new.akwad.qa';
   static const String baseUrl = '$hostUrl/api/method/jahiz.smart_api';
   static const Duration dioTimeout = Duration(milliseconds: 120000);
   static const String qatarInternationalCodeLabel = '+974';
