2abc8a261192933c05bd8fcf22212dedc60b9175
batoul-alani <batoule50@gmail.com>
2024-01-10 16:30:42 +0300

    fixing edit profile button

--- lib/features/profile/presentation/pages/edit_profile_page.dart
+++ lib/features/profile/presentation/pages/edit_profile_page.dart
@@ -31,106 +31,119 @@ class EditProfilePage extends StatelessWidget implements AutoRouteWrapper {
             final profileDetails = state.profileDetails;
             return Form(
               key: context.read<EditProfileCubit>().formKey,
-              child: Column(
+              child: Stack(
                 children: [
-                  TextFormField(
-                    initialValue: profileDetails.firstName,
-                    decoration: InputDecoration(
-                      label: Text(S.of(context).firstName),
-                    ),
-                    textInputAction: TextInputAction.next,
-                    validator:
-                        context.read<EditProfileCubit>().validator(context),
-                    onSaved: context.read<EditProfileCubit>().onSavedFirstName,
+                  Column(
+                    children: [
+                      TextFormField(
+                        initialValue: profileDetails.firstName,
+                        decoration: InputDecoration(
+                          label: Text(S.of(context).firstName),
+                        ),
+                        textInputAction: TextInputAction.next,
+                        validator:
+                            context.read<EditProfileCubit>().validator(context),
+                        onSaved:
+                            context.read<EditProfileCubit>().onSavedFirstName,
+                      ),
+                      const SizedBox(height: 12.0),
+                      TextFormField(
+                        initialValue: profileDetails.lastName,
+                        decoration: InputDecoration(
+                          label: Text(S.of(context).lastName),
+                        ),
+                        textInputAction: TextInputAction.next,
+                        validator:
+                            context.read<EditProfileCubit>().validator(context),
+                        onSaved:
+                            context.read<EditProfileCubit>().onSavedLastName,
+                      ),
+                      const SizedBox(height: 12.0),
+                      TextFormField(
+                        initialValue:
+                            profileDetails.mobileNo.replaceAll('974', ''),
+                        decoration: InputDecoration(
+                          label: Text(S.of(context).phone),
+                        ),
+                        textInputAction: TextInputAction.next,
+                        validator: context
+                            .read<EditProfileCubit>()
+                            .phoneValidator(context),
+                        onSaved:
+                            context.read<EditProfileCubit>().onSavedPhoneNumber,
+                      ),
+                      const SizedBox(height: 12.0),
+                      TextFormField(
+                        initialValue: profileDetails.email,
+                        decoration: InputDecoration(
+                          label: Text(S.of(context).email),
+                        ),
+                        textInputAction: TextInputAction.done,
+                        validator: context
+                            .read<EditProfileCubit>()
+                            .emailValidator(context),
+                        onSaved: context.read<EditProfileCubit>().onSavedEmail,
+                      ),
+                      const SizedBox(height: 30),
+                      BlocBuilder<EditProfileCubit, EditProfileState>(
+                        builder: (context, state) {
+                          if (state is EditProfileDeleteLoading) {
+                            return const Center(
+                                child: CircularProgressIndicator.adaptive());
+                          }
+                          return ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Theme.of(context)
+                                    .primaryColor
+                                    .withOpacity(0.05),
+                                elevation: 0.0,
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(5.0)),
+                                textStyle: const TextStyle(
+                                    fontSize: 18.0,
+                                    fontFamily: FontFamily.qatar),
+                                foregroundColor: AppColors.darkGray,
+                              ),
+                              onPressed: context
+                                  .read<EditProfileCubit>()
+                                  .deleteProfile,
+                              child: Text(S.of(context).deleteAccount));
+                        },
+                      ),
+                    ],
                   ),
-                  const SizedBox(height: 12.0),
-                  TextFormField(
-                    initialValue: profileDetails.lastName,
-                    decoration: InputDecoration(
-                      label: Text(S.of(context).lastName),
-                    ),
-                    textInputAction: TextInputAction.next,
-                    validator:
-                        context.read<EditProfileCubit>().validator(context),
-                    onSaved: context.read<EditProfileCubit>().onSavedLastName,
-                  ),
-                  const SizedBox(height: 12.0),
-                  TextFormField(
-                    initialValue: profileDetails.mobileNo.replaceAll('974', ''),
-                    decoration: InputDecoration(
-                      label: Text(S.of(context).phone),
-                    ),
-                    textInputAction: TextInputAction.next,
-                    validator: context
-                        .read<EditProfileCubit>()
-                        .phoneValidator(context),
-                    onSaved:
-                        context.read<EditProfileCubit>().onSavedPhoneNumber,
-                  ),
-                  const SizedBox(height: 12.0),
-                  TextFormField(
-                    initialValue: profileDetails.email,
-                    decoration: InputDecoration(
-                      label: Text(S.of(context).email),
-                    ),
-                    textInputAction: TextInputAction.done,
-                    validator: context
-                        .read<EditProfileCubit>()
-                        .emailValidator(context),
-                    onSaved: context.read<EditProfileCubit>().onSavedEmail,
-                  ),
-                  const SizedBox(height: 30),
-                  BlocBuilder<EditProfileCubit, EditProfileState>(
-                    builder: (context, state) {
-                      if (state is EditProfileDeleteLoading) {
-                        return const Center(
-                            child: CircularProgressIndicator.adaptive());
-                      }
-                      return ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: Theme.of(context)
-                                .primaryColor
-                                .withOpacity(0.05),
-                            elevation: 0.0,
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(5.0)),
-                            textStyle: const TextStyle(
-                                fontSize: 18.0, fontFamily: FontFamily.qatar),
-                            foregroundColor: AppColors.darkGray,
-                          ),
-                          onPressed:
-                              context.read<EditProfileCubit>().deleteProfile,
-                          child: Text(S.of(context).deleteAccount));
-                    },
-                  ),
-                  const Spacer(),
-                  BlocConsumer<EditProfileCubit, EditProfileState>(
-                    listener: (context, state) {
-                      if (state is EditProfileSuccess) {
-                        if (state.profileDetails.enabled == 0) {
-                          context.read<AuthCubit>().setUnauthenticated();
-                          context.popRoute();
-                        } else {
-                          context.read<AppSettingsCubit>().refresh();
-                          context.popRoute();
+                  Positioned(
+                    bottom: MediaQuery.of(context).viewInsets.bottom,
+                    left: 0.0,
+                    right: 0.0,
+                    child: BlocConsumer<EditProfileCubit, EditProfileState>(
+                      listener: (context, state) {
+                        if (state is EditProfileSuccess) {
+                          if (state.profileDetails.enabled == 0) {
+                            context.read<AuthCubit>().setUnauthenticated();
+                            context.popRoute();
+                          } else {
+                            context.read<AppSettingsCubit>().refresh();
+                            context.popRoute();
+                          }
+                        } else if (state is EditProfileError) {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(content: Text(state.message)));
                         }
-                      } else if (state is EditProfileError) {
-                        ScaffoldMessenger.of(context).showSnackBar(
-                            SnackBar(content: Text(state.message)));
-                      }
-                    },
-                    builder: (context, state) {
-                      if (state is EditProfileLoading) {
-                        return const Center(
-                            child: CircularProgressIndicator.adaptive());
-                      }
-                      return ElevatedButton(
-                          onPressed: () =>
-                              context.read<EditProfileCubit>().submit(),
-                          child: Text(S.of(context).updateProfile));
-                    },
+                      },
+                      builder: (context, state) {
+                        if (state is EditProfileLoading) {
+                          return const Center(
+                              child: CircularProgressIndicator.adaptive());
+                        }
+                        return ElevatedButton(
+                            onPressed: () =>
+                                context.read<EditProfileCubit>().submit(),
+                            child: Text(S.of(context).updateProfile));
+                      },
+                    ),
                   ),
-                  const SizedBox(height: 20),
+                  // const SizedBox(height: 20),
                 ],
               ),
             );
